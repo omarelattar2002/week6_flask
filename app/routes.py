4c58dab17b6c44998580a23a47b11e392541feb0
omar elattar <omarelattar2002@gmail.com>
2024-03-28 06:28:39 -0500

    the flask sql alchemy isnt launching

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,26 @@
-
+from flask import render_template, request
 from list.tasks import tasks_list
 from . import app
 
 
 @app.route("/")
 def index():
-    hello = "Hello world"
-    return hello
+    return render_template('index.html')
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    data = request.json
+
+    required_fields = ['first_name', 'last_name', 'username', 'email', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+        if missing_fields:
+            return {'error':f"{', '.join(missing_fields)} must be in the request body"}, 400
+        return 'This is the create user endpoint'
 
 @app.route("/tasks")
 def get_tasks():
@@ -18,3 +32,5 @@ def get_task(task_id):
         if task['id'] == task_id:
             return task
     return {'error':'The task you are looking for cannot be found'},404
+
+
